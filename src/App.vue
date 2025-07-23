1e61684364b7afd9e51e251ca2d7b5953fca2acf
Ronaldo Serra <ronaldo.serra@cencosud.cl>
2025-07-23 18:46:59 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+    <PokemonGame />
+</template>
+
+<script setup lang="ts">
+import PokemonGame from './modules/pokemon/pages/PokemonGame.vue';
+
+</script>
---
+++ src/modules/pokemon/pages/PokemonGame.vue
@@ -0,0 +1,25 @@
+<template>
+    <section v-if="isLoading" class="flex flex-col justify-center items-center w-screen h-screen">
+        <h1 class="text-3xl">Espere por favor</h1>
+        <h3 class="animate-pulse">Cargando Pokémons</h3>
+    </section>
+
+    <section v-else="isLoading" class="flex flex-col justify-center items-center w-screen h-screen">
+        <h1 class="m-5">¿Quién es este Pokémon?</h1>
+
+        <!-- Pokemon Picture -->
+        <PokemonPicture />
+
+        <!-- Pokemon Options -->
+        <PokemonOptions />
+
+    </section>
+</template>
+
+<script lang="ts" setup>
+import PokemonOptions from '../components/PokemonOptions.vue';
+import PokemonPicture from '../components/PokemonPicture.vue';
+import { usePokemonGame } from '../composables/usePokemonGame';
+
+const { gameStatus, isLoading } = usePokemonGame();
+</script>
